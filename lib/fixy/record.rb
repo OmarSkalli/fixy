0953623a29b42f4b3cc9cd09266f80650860567c
Kirill Klimuk <kirill@zenpayroll.com>
2015-04-20 10:56:40 -0700

    and again

--- lib/fixy/record.rb
+++ lib/fixy/record.rb
@@ -77,7 +77,7 @@ module Fixy
         current_position = 1
         current_record = 1
 
-        byte_record = record.bytes
+        byte_record = record.bytes.to_a
         while current_position <= record_length do
 
           field = record_fields[current_position]
@@ -87,7 +87,7 @@ module Fixy
           from   = field[:from] - 1
           to     = field[:to]   - 1
           method = field[:name]
-          value  = byte_record[from..to].to_a.pack('C*').force_encoding('utf-8')
+          value  = byte_record[from..to].pack('C*').force_encoding('utf-8')
 
           formatted_value = decorator.field(value, current_record, current_position, method, field[:size], field[:type])
           output << formatted_value
